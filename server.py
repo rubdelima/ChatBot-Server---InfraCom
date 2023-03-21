8a49bc6a5e19ad9d0142960aff4b690d7e338275
Rubens Lima <rubensndelima@hotmail.com>
2023-03-20 21:52:30 -0300

    poo + controle de dados

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 import datetime
-from threading import Thread
+from user import User
 
 # função para obter a hora atual
 def get_time() -> str:
@@ -24,31 +24,23 @@ cardapio = {'Sushi': 10,'Carne de Sol': 20}
 lista_opcoes = ['sair', 'cardapio', 'pedido', 'pagar', 'fatura']
 clients = {}
 
+
 thread_run = True
 
-def mainloop():
-    while thread_run:
-        data, client_address = server_socket.recvfrom(1024)
-        message = data.decode()
-        parts = message.split(':')
-        command = parts[0]
-        args = parts[1:]
-        match(command):
+def run_command(command, args, client_address, data):
+    match(command):
             case 'chefia':
-                args.append([]) # lista de pedidos 
-                args.append(0) #valor gasto
-                args.append(0) #valor pago
-                clients[client_address] = args
+                clients[client_address] = User(client_address, args)
                 print(f'{get_time()} Cliente conectado: {args[0]} (mesa {args[1]})')
                 server_socket.sendto(f'O que deseja fazer?{lista_opcoes}'.encode(), client_address)
 
             case 'sair':
                 # remove o cliente do dicionário de clientes se o valor gasto for igual ao valor pago
                 if client_address in clients:
-                    args = clients[client_address]
-                    if args[4] == args[3]:
+                    client = clients[client_address]
+                    if client.valor_pago == client.valor_gasto:
                         clients.pop(client_address)
-                        print(f'{get_time()} Cliente desconectado: {args[0]} (mesa {args[1]})')
+                        print(f'{get_time()} Cliente desconectado: {client.nome} (mesa {client.mesa})')
                         server_socket.sendto('Ok, você pode sair.'.encode(), client_address)
                     else:
                         server_socket.sendto('Você não pode sair, ainda tem uma fatura pendente.'.encode(), client_address)
@@ -61,62 +53,60 @@ def mainloop():
             case 'fatura':
                 # envia ao cliente a lista de pedidos e o valor total da fatura
                 if client_address in clients:
-                    args = clients[client_address]
-                    pedido_str = ', '.join(args[2])
-                    fatura_str = f'Valor gasto: {args[2]} reais\nPedidos: {pedido_str}'
-                    server_socket.sendto(fatura_str.encode(), client_address)
+                    server_socket.sendto((clients[client_address].get_fatura()).encode(), client_address)
 
             case 'pagar':
                 # atualiza o valor pago pelo cliente
                 if client_address in clients:
-                    args = clients[client_address]
-                    args[4] = args[3]
+                    clients[client_address].pay()
                     server_socket.sendto('Obrigado pelo pagamento.'.encode(), client_address)
 
             case 'pedido':
                 # recebe a mensagem perguntando quantos pedidos o cliente deseja fazer
                 server_socket.sendto('Quantos pedidos deseja fazer?'.encode(), client_address)
-                data, client_address = server_socket.recvfrom(1024)
-                num_pedidos = int(data.decode())
-
-                # recebe os nomes dos pedidos e registra no banco
-                pedidos = []
-                total_pedido = 0
-                for i in range(num_pedidos):
-                    server_socket.sendto('Digite o nome do pedido:'.encode(), client_address)
-                    data, client_address = server_socket.recvfrom(1024)
-                    nome_pedido = data.decode()
-                    if nome_pedido in cardapio:
-                        pedidos.append(nome_pedido)
-                        total_pedido += cardapio[nome_pedido]
-                    else:
-                        server_socket.sendto('Pedido inválido.'.encode(), client_address)
-
-                # atualiza o valor gasto pelo cliente e a lista de pedidos
-                if client_address in clients:
-                    args = clients[client_address]
-                    args[3] += total_pedido
-                    args[2].extend(pedidos)
-                    server_socket.sendto(f'Pedido registrado. Total a pagar: {args[3]} reais'.encode(), client_address)
+                clients[client_address].fase = 'pedido1'
 
             case _:
-                server_socket.sendto('Código inválido'.encode(), client_address)
+                match(clients[client_address].fase):
+                    case 'pedido1':
+                        try:
+                            clients[client_address].novo_pedido_qnt = int(data.decode())
+                            server_socket.sendto('Digite o nome do pedido:'.encode(), client_address)
+                            clients[client_address].fase = 'pedido2'
+                        except:
+                            server_socket.sendto('Informe um valor válido'.encode(), client_address)
+                    
+                    case 'pedido2':
+                        nome_pedido = data.decode()
+                        if nome_pedido in cardapio:
+                            clients[client_address].novo_pedido.append(nome_pedido)
+                            clients[client_address].valor_gasto += cardapio[nome_pedido]
+                            pedidos_restantes = clients[client_address].novo_pedido_qnt - len(clients[client_address].novo_pedido)
+                            if pedidos_restantes >0:
+                                mensagem = f'Pedido de {nome_pedido} registrado, você têm {pedidos_restantes} pedidos.'
+                            else:
+                                mensagem = f'Pedido de {nome_pedido} registrado, o total é de {sum([cardapio[i] for i in clients[client_address].novo_pedido])}'
+                                clients[client_address].atualizar_pedidos()
+                            server_socket.sendto(mensagem.encode(), client_address)
+                        else:
+                            server_socket.sendto('Pedido inválido.'.encode(), client_address)
+                        
+                    case _:
+                        server_socket.sendto('Código inválido'.encode(), client_address)
                 # operação inválida
 
-        for i in clients.keys():
-            print(f'{i} : {clients[i]}')
 
 if __name__ == '__main__':
-    
-    thr = Thread(target=mainloop)
-    thr.run()
-    
     while True:
-        command_server = input()
-        if command_server == 'exit':
-            thread_run = False
-            break
-        elif command_server == 'show':
-            for i in clients.keys():
-                print(f'{i} : {clients[i]}')
+        data, client_address = server_socket.recvfrom(1024)
+        message = data.decode()
+        parts = message.split(':')
+        command = parts[0]
+        args = parts[1:]
+        
+        run_command(command, args, client_address, data)
+        
+        for i in clients.keys():
+            print(clients[i])
+
 
