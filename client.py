b43d586a7fd38aa3a1150a5e256344486766deeb
Felipe Neiva <felipeneivals@gmail.com>
2023-04-06 12:48:30 -0300

    refactor: added comments

--- client.py
+++ client.py
@@ -8,37 +8,48 @@ BUFFER_SIZE = 1024
 server_address = ('localhost', 5000)
 client_address = ('localhost', 0)
 
+
 def send_file(sock, filename):
-    filesize = os.path.getsize(filename)    # tamanho do arquivo
+    # Obtém o tamanho do arquivo
+    filesize = os.path.getsize(filename)
     print(f"o tamanho do arquivo enviado para o servidor e {filesize}")
     data = f"{os.path.basename(filename)}|{filesize}"
-    sock.sendto(data.encode('utf-8'), server_address)   # codifica em bytes e envia para o servidor
+    # Codifica os dados e envia para o servidor
+    sock.sendto(data.encode('utf-8'), server_address)
+    # Recebe uma mensagem de confirmação (ack) do servidor
     ack, _ = sock.recvfrom(BUFFER_SIZE)
 
     with open(filename, 'rb') as f:
         with tqdm(total=filesize, desc=f'Sending {filename}', unit='B', unit_scale=True) as pbar:
             while True:
-                data = f.read(BUFFER_SIZE)  # lê 1024 bytes por vez
+                # Lê 1024 bytes por vez
+                data = f.read(BUFFER_SIZE)
                 if not data:
                     break
-                sock.sendto(data, server_address)   # envia os bytes
+                # Envia os bytes para o servidor
+                sock.sendto(data, server_address)
+                # Atualiza a barra de progresso com o número de bytes enviados
                 pbar.update(len(data))
         print(f"{filename} sent")
         f.close()
-    
+
     return filesize
+
 
 def receive_file(sock, filename, filesize):
     client_directory = f"client_files/received"
 
     if not os.path.exists(client_directory):
         os.makedirs(client_directory)
-    
+
     with open(f"{client_directory}/{filename}", 'wb') as f:
         received = 0
         while received < filesize:
+            # Recebe os dados do servidor
             data, _ = sock.recvfrom(BUFFER_SIZE)
+            # Escreve os dados no arquivo
             f.write(data)
+            # Incrementa o número de bytes recebidos
             received += len(data)
         print(f"{filename} received from server")
         f.close()
@@ -46,15 +57,20 @@ def receive_file(sock, filename, filesize):
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.bind(client_address)   # vincula com o endereço de destino
+        # Vincula o socket com o endereço de destino
+        sock.bind(client_address)
         print(f"Client started on {sock.getsockname()}")
 
-        filename = askopenfilename()    # seleciona o arquivo a ser enviado
+        # abre uma janela de para selecionar o arquivo a ser enviado
+        filename = askopenfilename()
 
-        filesize = send_file(sock, filename)   # envia o arquivo
+        # envia o arquivo para o servidor e obtém o tamanho do arquivo
+        filesize = send_file(sock, filename)
         # TODO: devemos pedir para o servidor o novo filesize, ou usar o mesmo?
         print(f"o tamanho do arquivo que o client espera receber e {filesize}")
+        # recebe o arquivo do servidor
         receive_file(sock, os.path.basename(filename), filesize)
+
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -2,12 +2,21 @@ import socket
 import os
 from threading import Thread
 from tqdm import tqdm
+
+# Define o tamanho do buffer utilizado na transferência de dados
 BUFFER_SIZE = 1024
 
+# Define o endereço do servidor
 server_address = ('localhost', 5000)
+
+# Cria um dicionário para armazenar a identificação de cada cliente conectado
 clients = {}
+
+# Cria uma lista para armazenar as threads criada
 thread_list = []
 
+
+# Função que recebe um arquivo enviado por um cliente e o salva no diretório do cliente correspondente
 def receive_file(sock, address, filename, filesize):
     received = 0
     client_directory = f"server_files/{clients[address]}"
@@ -20,12 +29,14 @@ def receive_file(sock, address, filename, filesize):
             received += len(data)
         print(f"{filename} received from {address}")
         f.close()
-    
+
     return client_directory
 
+
+# Função que envia o arquivo de volta para o cliente
 def send_file(sock, filename, address):
     filesize = os.path.getsize(filename)
-    
+
     with open(filename, 'rb') as f:
         with tqdm(total=filesize, desc=f'Sending {os.path.basename(filename)}', unit='B', unit_scale=True) as pbar:
             while True:
@@ -39,24 +50,39 @@ def send_file(sock, filename, address):
         print(f"{os.path.basename(filename)} sent")
         f.close()
 
+
 if __name__ == '__main__':
+    # Cria um socket UDP
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.bind(server_address)   # vincula com o endereço do servidor
+        # Vincula o socket com o endereço do servidor
+        sock.bind(server_address)
         print(f"Server started on {server_address}")
 
+        # Entra em um loop para receber arquivos de clientes
         while True:
+            # Recebe dados enviados por um cliente e o endereço do cliente que enviou os dados
             data, address = sock.recvfrom(BUFFER_SIZE)
+
+            # Verifica se o cliente que enviou os dados já está registrado no dicionário clients
             if address not in clients:
                 clients[address] = len(clients) + 1
 
+            # Extrai o nome do arquivo e o tamanho do arquivo dos dados recebidos
             filename, filesize = data.decode('utf-8').split('|')
             filesize = int(filesize)
             print(f"o tamanho do arquivo que o client vai enviar e {filesize}")
 
+            # Envia uma mensagem de confirmação ('ack') para o cliente
             sock.sendto('ack'.encode('utf-8'), address)
+
+            # Cria uma nova thread para receber o arquivo do cliente e adiciona a thread na lista
             thread_list.append(Thread(target=receive_file, args=(sock, address, filename, filesize)))
 
+            # Recebe o arquivo enviado pelo cliente e armazena o caminho do arquivo no diretório do cliente
             directory = receive_file(sock, address, filename, filesize)
             filename = f"{directory}/{filename}"
             print(f"o tamanho do arquivo que o servidor vai enviar e {filesize}")
+
+            # Envia o arquivo de volta para o cliente
+
             send_file(sock, filename, address)
